693197d53f02d94179ac4d9be9c1bda63de4bbbe
Shrinidhi G <65757568+Shrinidhig29@users.noreply.github.com>
2022-05-09 15:44:34 +0530

    Assignment 2
    String, Objects and list objects.

---
+++ assign5.py
@@ -0,0 +1,13 @@
+"""Write a python program to reverse a word after accepting the input from the user."""
+
+word = input("Enter the input: ")
+rev_word = word[::-1]
+print(rev_word)
+
+# def method
+"""
+def rev_wrd(rev):
+    return rev[::-1]
+wrd = input("enter the word: ")
+re_wrd = rev_wrd(wrd)
+print(re_wrd)"""
